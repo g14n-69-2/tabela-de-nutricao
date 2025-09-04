fcc2ac925ded71e204b1ce2bb167cd6fd390a800
g14n-69 <gianluca.siqueira@escola.pr.gov.br>
2025-09-04 09:00:06 -0300

    Add files via upload

--- tabelaNutricao-main/js/principal.js
+++ tabelaNutricao-main/js/principal.js
@@ -1,29 +1,44 @@
 //muda o nome a partir do seletor de classe
-var pacientes = document.querySelector(".subtitulo");
-pacientes.textContent = "Meus Melecas";
+var subtitulo = document.querySelector(".subtitulo");
+subtitulo.textContent = "Meus Melecas";
 var pipipupu = document.querySelector(".titulo");
 pipipupu.textContent = "Dr. Pipipupu Nutrição";
 
-//acessar a tag TR -> paciente Paulo
-var paciente = document.querySelector("#primeiro-meleca");
-//seleciona o conteúdo PESO da tag
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+//acessar a tag TR -> meleca Paulo
+var paciente = document.querySelector(".meleca");
 
-//seleciona a altura do meleca
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for(var i = 0; i < paciente.length; i++){
 
-//calcula o imc do meleca
-var imc = peso / (altura * altura);
-var tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent = imc;
 
-if(peso < 0 || peso > 1000){
-    alert("Peso inválido");
+    //seleciona o PESO do meleca
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    //seleciona a ALTURA do meleca
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    //calcula o IMC do meleca
+    var imc = peso / (altura * altura);
+
+        //variáveis com valor TRUE
+        var pesoValido = true;
+        var alturaValida = true;
+
+    //condicionais
+    if(pesoValido && alturaValida){
+     var tdIMC = paciente.querySelector(".info-imc");
+     tdIMC.textContent = imc;
+    }
+    if(peso <= 0 || peso >= 1000){
+     pesoValido = false;
+     console.log("Peso inválido");
+     tdIMC.textContent = "Peso Inválido";
+    }
+    if(altura <= 0 || altura >= 3.00){
+     alturaValida = false;
+     console.log("alto demais, seu muco");
+     tdAltura.textContent = "Peso Inválido";
+    }
+
 }
-if(altura < 0 || altura > 2.99){
-    alert("Altura inválida");
-}
-
-
